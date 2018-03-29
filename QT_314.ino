a8031c6d1416f65fb2f89a0330c43d39e9ed68f9
tr4nt0r <manni@zapto.de>
2018-03-29 19:56:58 +0200

    changed motorpins and deactivated LED-blink Timer

--- QT_314.ino
+++ QT_314.ino
@@ -2,6 +2,8 @@
 #include "Driving.h"
 #include <NewPing.h>
 
+#define MAX_SPEED 225
+#define MIN_SPEED 50
 
 const uint8_t TRIGGER_PIN = 10;
 const uint8_t ECHO_PIN = 11;
@@ -9,11 +11,11 @@ const uint8_t ECHO_PIN = 11;
 const uint8_t LED_BLUE = 2;
 const uint8_t LED_RED = 4;
 
-const uint8_t PIN_MOTOR1_PWM = 8;
-const uint8_t PIN_MOTOR1_DIR = 9;
+const uint8_t PIN_MOTOR1_PWM = 10;
+const uint8_t PIN_MOTOR1_DIR = 8;
 
-const uint8_t PIN_MOTOR2_PWM = 7;
-const uint8_t PIN_MOTOR2_DIR = 6;
+const uint8_t PIN_MOTOR2_PWM = 9;
+const uint8_t PIN_MOTOR2_DIR = 7;
 
 volatile bool toggleLED = 1;
 
@@ -27,36 +29,31 @@ void setup()
 	pinMode(LED_BLUE, OUTPUT);
 	pinMode(LED_RED, OUTPUT);
 	
-	Timer1.initialize(500000);
-	Timer1.attachInterrupt(blinkLED);
+	//Timer1.initialize(500000);
+	//Timer1.attachInterrupt(blinkLED);
 
 	Driving.init(PIN_MOTOR2_PWM, PIN_MOTOR2_DIR, PIN_MOTOR1_PWM, PIN_MOTOR1_DIR);	
+	
 	Driving.drive(MAX_SPEED, DIR_FORWARD);
-	Serial.println("Start driving forward...");
-	delay(1000);
-	
-	Driving.drive(MAX_SPEED, DIR_BACKWARD);
-	Serial.println("Start driving backwards...");
-	delay(1000);
-
-	Driving.turn(MAX_SPEED, DIR_LEFT);
-	Serial.println("Start turning left...");
 	delay(5000);
-	
 	Driving.halt();
-	Serial.println("Stop driving...");
+	delay(1000);
+	Driving.drive(MIN_SPEED, DIR_FORWARD);
+	delay(5000);
+	Driving.halt();
+	delay(1000);
+	Driving.drive(MAX_SPEED, DIR_BACKWARD);
+	delay(5000);
+	Driving.halt();
 }
 
 void loop()
 {
 
-
-	
 }
 
 
 void blinkLED() {
-	//Serial.println(toggleLED);
 	if (Driving.isDriving()) {
 		digitalWrite(LED_BLUE, toggleLED);
 		toggleLED ^= 1;
